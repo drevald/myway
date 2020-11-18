13f53464e3a9ee1d4621951c1c0a992e038c090f
denis <oudun@mail.ru>
2020-11-18 18:36:13 +0300

    changes

--- myway/settings.py
+++ myway/settings.py
@@ -3,3 +3,16 @@ PORT = 8000
 SECRET_KEY = 1
 DEBUG = True
 ROOT_URLCONF = "myway.urls"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myway',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    },
+}
+INSTALLED_APPS = [
+    'myway'
+]
