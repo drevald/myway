baaa68096ee2341177bcf31961f3d0d9f4f9c331
denis <oudun@mail.ru>
2020-11-20 01:00:29 +0300

    Forms added

--- myway/urls.py
+++ myway/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 import myway.views as views
 
+app_name = 'myway'
+
 urlpatterns = [
     path('', views.index),
-    path('trips/', views.trips_list),
-    path('trips/create', views.trips_create),
-    path('trips/<int:pk>/delete', views.trips_delete),
-    path('trips/<int:pk>/details', views.trips_details)
+    path('trips/', views.trips_list, name = 'trips'),
+    path('trips/create', views.trips_create, name = 'create'),
+    path('trips/<int:pk>/delete', views.trips_delete, name = 'delete'),
+    path('trips/<int:pk>/details', views.trips_details, name = 'details')
 ]
--- myway/views.py
+++ myway/views.py
@@ -1,21 +1,33 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from . import models
+from . import forms
 
 def index(request):
     return HttpResponse('hi')
 
 def trips_list(request):
-    trips = models.Trip.objects
+    trips = models.Trip.objects.all()
     template = loader.get_template('trips.html')
     context = {'trips' : trips}
     return HttpResponse(template.render(context, request))
 
 def trips_create(request):
-    return HttpResponse('trip_create')
+    trip = models.Trip()
+    form = forms.TripForm(request.POST or None, instance=trip)
+    if form.is_valid():
+        form.save()        
+        return HttpResponseRedirect(reverse('trips'))
+    template = 'trip_new.html'
+    context = {'form':form}
+    return render(request, template, context)
 
-def trips_delete(request):
+def trips_delete(request, pk):
     return HttpResponse('trip_delete')
 
-def trips_details(request):
+def trips_details(request, pk):
     return HttpResponse('trip_details')
