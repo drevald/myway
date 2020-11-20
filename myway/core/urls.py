af435032e87e11ebc9854f597c4aab3dc2893550
denis <oudun@mail.ru>
2020-11-20 14:34:03 +0300

    some url fixes

--- myway/core/urls.py
+++ myway/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
+    path('', views.index, name = 'index'),
     path('trips', views.trips_list, name = 'trips_list'),
     path('trips/create', views.trips_create, name = 'trips_create'),
     path('trips/<int:pk>/delete', views.trips_delete, name = 'trips_delete'),
