e1a576d857ddc1020c58e9a3fb6ca7ca908397da
denis <oudun@mail.ru>
2020-11-22 14:17:17 +0300

    points, objects and photos added

--- myway/core/urls.py
+++ myway/core/urls.py
@@ -4,7 +4,29 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('trips', views.TripsPageView.as_view(), name='trips'),
-    path('trips/create', views.TripsCreateView.as_view(), name = 'trips_create'),
+    path('', views.HomePageView.as_view(), name='home'),    
+    
+    path('trips', views.TripsView.as_view(), name='trips'),
+    path('trips/create', views.TripCreateView.as_view(), name = 'trip_create'),
+    path('trips/<pk>/delete', views.TripDeleteView.as_view(), name = 'trip_delete'),
+    path('trips/<pk>/edit', views.TripEditView.as_view(), name = 'trip_edit'),
+    
+    path('points', views.PointsView.as_view(), name='points'),
+    path('point/create', views.PointCreateView.as_view(), name = 'point_create'),
+    path('point/<pk>/delete', views.PointDeleteView.as_view(), name = 'point_delete'),
+    path('point/<pk>/edit', views.PointEditView.as_view(), name = 'point_edit'),
+
+    path('objects', views.ObjectsView.as_view(), name='objects'),
+    path('object/create', views.ObjectCreateView.as_view(), name = 'object_create'),
+    path('object/<pk>/delete', views.ObjectDeleteView.as_view(), name = 'object_delete'),
+    path('object/<pk>/edit', views.ObjectEditView.as_view(), name = 'object_edit'),
+
+    path('photos', views.PhotosView.as_view(), name='photos'),
+    path('photo/create', views.PhotoCreateView.as_view(), name = 'photo_create'),
+    path('photo/<pk>/delete', views.PhotoDeleteView.as_view(), name = 'photo_delete'),
+    path('photo/<pk>/edit', views.PhotoEditView.as_view(), name = 'photo_edit')
+
 ]
+
+
 
--- myway/core/views.py
+++ myway/core/views.py
@@ -1,19 +1,101 @@
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
 from . import models
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
-class TripsPageView(ListView):
+
+class TripsView(ListView):
     model = models.Trip
     template_name = 'trips.html'
     context_object_name = 'all_trips_list' # new    
 
-class TripsCreateView(CreateView):
+class TripCreateView(CreateView):
     model = models.Trip
     template_name = 'trip_new.html'
+    fields = ('name',)
+    success_url = reverse_lazy('core:trips')
+
+class TripDeleteView(DeleteView):
+    model = models.Trip
+    template_name = 'trip_delete.html'
+    success_url = reverse_lazy('core:trips')    
+
+class TripEditView(UpdateView):
+    model = models.Trip
+    template_name = 'trip_edit.html'
+    fields = ('name',)
+    success_url = reverse_lazy('core:trips')        
+
+
+class PointsView(ListView):
+    model = models.ShowPoint
+    template_name = 'points.html'
+    context_object_name = 'points_list'
+
+class PointCreateView(CreateView):
+    model = models.ShowPoint
+    template_name = 'point_new.html'
+    fields = ('latitude', 'longitude')
+    success_url = reverse_lazy('core:points')
+
+class PointDeleteView(DeleteView):
+    model = models.ShowPoint
+    template_name = 'point_delete.html'
+    success_url = reverse_lazy('core:points')
+
+class PointEditView(UpdateView):
+    model = models.ShowPoint
+    template_name = 'point_edit.html'
+    fields = ('latitude','longitude')
+    success_url = reverse_lazy('core:points')
+
+
+class ObjectsView(ListView):
+    model = models.ShowObject
+    template_name = 'objects.html'
+    context_object_name = 'objects_list'
+
+class ObjectCreateView(CreateView):
+    model = models.ShowObject
+    template_name = 'object_new.html'
+    fields = ('name',)
+    success_url = reverse_lazy('core:objects')
+
+class ObjectDeleteView(DeleteView):
+    model = models.ShowObject
+    template_name = 'object_delete.html'
+    success_url = reverse_lazy('core:objects')
+
+class ObjectEditView(UpdateView):
+    model = models.ShowObject
+    template_name = 'object_edit.html'
+    fields = ('name',)
+    success_url = reverse_lazy('core:objects')
+
+class PhotosView(ListView):
+    model = models.Photo
+    template_name = 'photos.html'
+    context_object_name = 'photos_list'
+
+class PhotoCreateView(CreateView):
+    model = models.Photo
+    template_name = 'photo_new.html'
     fields = '__all__'
-    url = reverse('core:trips')
+    success_url = reverse_lazy('core:photos')
+
+class PhotoDeleteView(DeleteView):
+    model = models.Photo
+    template_name = 'photo_delete.html'
+    success_url = reverse_lazy('core:photos')
+
+class PhotoEditView(UpdateView):
+    model = models.Photo
+    template_name = 'photo_edit.html'
+    fields = '__all__'
+    success_url = reverse_lazy('core:photos')
